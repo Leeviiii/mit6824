9a4bea35594df480e937f8daf51ef1eff6ed16a4
杨飞 <yangfei@yangfeideMacBook-Pro.local>
2018-11-08 18:24:36 +0800

    Part III: Distributing MapReduce tasks Part IV: Handling worker failures

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -1,6 +1,9 @@
 package mapreduce
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 //
 // schedule() starts and waits for all tasks in the given phase (Map
@@ -32,5 +35,66 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 	//
 	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
 	//
+
+	// the code is not correct why??? go test -run Basic
+	/*var wait_group sync.WaitGroup;
+	for taskNumber := 0; taskNumber < ntasks;taskNumber++ {
+		var mapFile string
+		if phase == mapPhase {
+			mapFile = mapFiles[taskNumber]
+		} else {
+			mapFile = ""
+		}
+		wait_group.Add(1)
+		go func(mapFile string) {
+			defer wait_group.Done()
+			for {
+				workerAddress := <-registerChan
+				fmt.Printf("get the registerChan %s\n", workerAddress)
+				args := DoTaskArgs{JobName: jobName, File: mapFile, Phase: phase, TaskNumber: taskNumber, NumOtherPhase: n_other}
+				ok := call(workerAddress, "Worker.DoTask", args, nil)
+				if ok == false {
+					fmt.Printf("DoWorker: RPC %s dotask error\n", workerAddress)
+				} else {
+					go func(){registerChan<-workerAddress}()
+					//registerChan<-workerAddress
+					break
+				}
+			}
+		}(mapFile)
+	}
+	wait_group.Wait()
+	fmt.Printf("Schedule: %v phase done\n", phase)*/
+    /*
+     * wait_group 控制着整个这次schedule 必须都完成才可以
+     *
+     * go func(){registerChan<-workerAddress}() 这里放回很关键，这里不放回下次就没办法在渠道workerAddress
+     * 就没办法并行。 我第一个版本就没有放回，导致串行，test偶尔会爆出有一些节点没有任何task的错误
+     */
+	var wait_group sync.WaitGroup;
+	for taskNumber := 0; taskNumber < ntasks;taskNumber++ {
+		args := DoTaskArgs{JobName: jobName, Phase: phase, TaskNumber: taskNumber, NumOtherPhase: n_other}
+		if phase == mapPhase {
+			args.File = mapFiles[taskNumber]
+		}
+		wait_group.Add(1)
+		go func() {
+			defer wait_group.Done()
+			for {
+				workerAddress := <-registerChan
+				fmt.Printf("get the registerChan %s\n", workerAddress)
+				ok := call(workerAddress, "Worker.DoTask", args, nil)
+				if ok == false {
+					fmt.Printf("DoWorker: RPC %s dotask error\n", workerAddress)
+				} else {
+					go func(){registerChan<-workerAddress}()
+					//registerChan<-workerAddress
+					break
+				}
+			}
+		}()
+	}
+	wait_group.Wait()
 	fmt.Printf("Schedule: %v phase done\n", phase)
+
 }
